8d1cf1a12ad8427ebaaf309696d4183fb9ebf08c
dliew11 <114978587+dliew11@users.noreply.github.com>
2024-10-24 12:47:14 +0800

    Update lab8.2.js

--- lab8.2.js
+++ lab8.2.js
@@ -1,5 +1,4 @@
 function init() {
-    
     var w = 500;
     var h = 300;
 
@@ -8,90 +7,100 @@ function init() {
                         .translate([w / 3, h / 2])
                         .scale(2450);
 
-    // Set up the path
     var path = d3.geoPath()
                 .projection(projection);
 
     var color = d3.scaleQuantize()
-                .range(["#f7fbff", "#c6dbef", "#6baed6", "#2171b5", "#08306b"])
+                .range(["#f7fbff", "#c6dbef", "#6baed6", "#2171b5", "#08306b"]);
 
     var svg = d3.select("#map")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
 
-    // Reading the data from CSV file
+    // Create a div for the tooltip
+    var tooltip = d3.select("body").append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0)
+                    .style("position", "absolute")
+                    .style("background-color", "white")
+                    .style("border", "solid")
+                    .style("border-width", "1px")
+                    .style("border-radius", "5px")
+                    .style("padding", "5px");
+
     d3.csv("VIC_LGA_unemployment.csv").then(function(data) {
         color.domain([
             d3.min(data, function(d) { return +d.unemployed; }),
             d3.max(data, function(d) { return +d.unemployed; })
         ]);
     
-    d3.json("https://raw.githubusercontent.com/dliew11/COS30045/refs/heads/main/LGA_VIC.json").then(function(json) {
+        d3.json("https://raw.githubusercontent.com/dliew11/COS30045/refs/heads/main/LGA_VIC.json").then(function(json) {
 
-        // Merge the ag. data and GeoJSON
-        // Loop through once for each ag. data value
-        for (var i = 0; i <data.length; i++) {
-            
-            // Grab state name
-            var dataLGA = data[i].LGA;
+            for (var i = 0; i < data.length; i++) {
+                var dataLGA = data[i].LGA;
+                var dataValue = parseFloat(data[i].unemployed);
 
-            // Grab data value, and convert from staeting to float
-            var dataValue = parseFloat(data[i].unemployed);
+                for (var j = 0; j < json.features.length; j++) {
+                    var jsonLGA = json.features[j].properties.LGA_name;
 
-            // Find the corresponding state inside the GeoJSON
-            for (var j = 0; j < json.features.length; j++) {
-
-                var jsonLGA = json.features[j].properties.LGA_name;
-
-                if (dataLGA == jsonLGA) {
-
-                    //Copy the data value ito the JSON
-                    json.features[j].properties.value = dataValue;
-
-                    //Stop looking through the JSON
-                    break;
+                    if (dataLGA == jsonLGA) {
+                        json.features[j].properties.value = dataValue;
+                        break;
+                    }
                 }
             }
-        }
-        svg.selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .style("fill", function(d) {
-            // Get data value
-            var value = d.properties.value;
 
-                if (value) {
-                    // If value exiest
-                    return color(value);
-                } else {
-                    // If value is undefined
-                    return "#ccc";
-                }
-            });
-
-        //Load in cities data
-        d3.csv("VIC_city.csv").then(function(data) {
-            svg.selectAll("circle")
-                .data(data)
+            svg.selectAll("path")
+                .data(json.features)
                 .enter()
-                .append("circle")
-                .attr("cx", function(d) {
-                    return projection([d.lon, d.lat]) [0];
-                })
-                .attr("cy", function(d) {
-                    return projection([d.lon, d.lat]) [1];
-                })
-                .attr("r", 5)
-                .style("stroke", "#000")
-                .style("stroke-width", 0.5);
+                .append("path")
+                .attr("d", path)
+                .style("fill", function(d) {
+                    var value = d.properties.value;
+                    return value ? color(value) : "#ccc";
+                });
 
+            // Load in cities data
+            d3.csv("VIC_city.csv").then(function(data) {
+                svg.selectAll("circle")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function(d) {
+                        return projection([d.lon, d.lat])[0];
+                    })
+                    .attr("cy", function(d) {
+                        return projection([d.lon, d.lat])[1];
+                    })
+                    .attr("r", 5)
+                    .style("stroke", "#000")
+                    .style("stroke-width", 0.5)
+                    .style("fill", "red")
+                    .on("mouseover", function(event, d) {
+                        // Log the data object to inspect its structure
+                        console.log(d);
+
+                        // Show tooltip, check if the city column is correctly named
+                        tooltip.transition()
+                            .duration(200)
+                            .style("opacity", 0.9);
+                        tooltip.html("City: " + d.city)  // Adjust 'd.city' to match your CSV file column
+                            .style("left", (event.pageX + 10) + "px")
+                            .style("top", (event.pageY - 20) + "px");
+                    })
+                    .on("mousemove", function(event) {
+                        tooltip.style("left", (event.pageX + 10) + "px")
+                               .style("top", (event.pageY - 20) + "px");
+                    })
+                    .on("mouseout", function() {
+                        tooltip.transition()
+                            .duration(500)
+                            .style("opacity", 0);
+                    });
             });
         });
     });
-
 }
 
 window.onload = init;
